051dddd379d0f862bd75616d62e0feab460df856
Connor <connorskorburg@gmail.com>
2021-02-05 12:10:57 -0600

    fixed responsiveness for navigation

--- client/src/components/layout/BottomNav.tsx
+++ client/src/components/layout/BottomNav.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import NavLink from '../reusables/NavLink';
 
 
 const BottomNav = () => {
+  const content = useSelector((state: any) => state.activeContent);
+
   return (
     <nav className="bottom-nav absolute bottom-0 w-full flex text-md">
-      <NavLink active={true} name="Applications"type="bottom"  />
-      <NavLink active={false} name="Interviews" type="bottom" />
-      <NavLink active={false} name="Network" type="bottom" />
+      <NavLink active={content.applications} name="Applications"type="bottom"  />
+      <NavLink active={content.interviews} name="Interviews" type="bottom" />
+      <NavLink active={content.network} name="Network" type="bottom" />
     </nav>
   );
 }
